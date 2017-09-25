d28033c19225b8ceef5e1909b5adb847147831f2
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2017-09-26 00:06:08 +0300

    First commit

---
+++ log_failed_logins.php
@@ -0,0 +1,31 @@
+<?php
+
+require 'vendor/autoload.php';
+
+class log_failed_logins extends rcube_plugin
+{
+    public function init()
+    {
+        $this->add_hook('login_failed', array($this, 'login_failed'));
+    }
+
+    public function login_failed(array $args)
+    {
+        $username = $args['user'] ?? '<unknown user>';
+        $hostname = $args['host'] ?? '<unknown host>';
+
+        openlog('roundcube-login', LOG_PID, LOG_USER);
+
+        $date   = gmdate('Y-m-d H:i:s');
+        $ip     = $_SERVER['REMOTE_ADDR']     ?? '-';
+        $method = $_SERVER['REQUEST_METHOD']  ?? '-';
+        $proto  = $_SERVER['SERVER_PROTOCOL'] ?? '-';
+        $host   = $_SERVER['HTTP_HOST']       ?? '-';
+        $uri    = $_SERVER['REQUEST_URI']     ?? '-';
+        $ref    = $_SERVER['HTTP_REFERER']    ?? '-';
+        $ua     = $_SERVER['HTTP_USER_AGENT'] ?? '-';
+
+        $message = "{$ip} {$host} [{$date}+0000] \"{$method} {$uri} {$proto}\" \"{$ref}\" \"{$ua}\" \"{$username}\" \"{$hostname}\"";
+        syslog(LOG_WARNING, $message);
+    }
+}
